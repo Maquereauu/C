5fdc4c0cc73d51bb7a7d87b9a400078a8a1d3453
unknown <gcharlesnicolai@gamingcampus.local>
2023-02-21 15:31:48 +0100

    test

--- C/C.c
+++ C/C.c
@@ -2,26 +2,131 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Case {
     int isBomb;
     int bombsNearby;
-    int isQuestionMarked;
+    int isFlaged;
     int isClicked;
 } Case;
 
-
+void Init();
+void ShowGrid();
 int main()
 {
+    int c = 0;
+    Case EmptyCase[10][10];
+    Init(EmptyCase);
+    ShowGrid(EmptyCase);
+}
+void Init(Case EmptyCase[10][10]) {
     int x;
     int y;
-    for (x=0;x<10 ;x++ ) {
+    int i;
+    int j;
+    int BombCount = 0;
+    int bombsnear = 0;
+    while (BombCount < 5) {
+        for (x = 0; x < 10; x++) {
+            for (y = 0; y < 10; y++) {
+                int randomBomb = (rand() % 10);
+                if (randomBomb == 0) {
+                    EmptyCase[x][y].isBomb = 1;
+                    BombCount++;
+                }
+                else {
+                    EmptyCase[x][y].isBomb = 0;
+                }
+
+            }
+        }
+    }
+    for (x = 0; x < 10; x++) {
         for (y = 0; y < 10; y++) {
-            printf("[]");
+            EmptyCase[x][y].isClicked = 0;
+            if (EmptyCase[x][y].isBomb == 0) {
+                for (i = 0; i < 3; i++) {
+                    for (j = 0; j < 10; j++) {
+                        if (EmptyCase[i][j].isBomb == 1) {
+                            bombsnear++;
+                        }
+                    }
+                }
+            }
+            int bombsnear = 0;
+        }
+    }
+}
+
+void ShowGrid(Case CaseList[10][10]) {
+    int x;
+    int y;
+    for (x = -1; x < 10; x++) {
+        for (y = -1; y < 10; y++) {
+            if (x == -1 || y == -1) {
+                if (x == -1) {
+                    if(y == -1){
+                        printf("salut %d  ", y);
+                    }
+                }
+                else if (y == -1) {
+                    printf("%d ", x);
+                }
+            }else if (CaseList[x][y].isClicked == 0) {
+                if (CaseList[x][y].isFlaged == 1) {
+                    printf("F");
+                }
+                else {
+                    printf(" ? ");
+                }
+            }
+            else {
+                if (CaseList[x][y].isClicked == 1) {
+                    if (CaseList[x][y].bombsNearby > 0) {
+                        printf("%d", CaseList[x][y].bombsNearby);
+                    }
+                    else if (CaseList[x][y].isBomb == 1) {
+                        printf("B");
+                    }
+                    else {
+                    }
+                }
+            }
         }
         printf("\n");
     }
 }
+
+//void flag(){ 
+//    //input ("voulez vous mettre un drapeau :")
+//    //if answer = O 
+//        //return 1 en x , y 
+//        
+//    //else 
+//        //return input ("Qu'elle case voulez vous marquer :")
+//    
+//}
+
+
+//int VerifWin(){ 
+//    //si case clické != isbomb 
+//        //return 0 
+//    //sinon 
+//        //return ""
+//
+//}
+//int win{
+//    //si VerifWin = 0 
+//        //return Win  
+//    //sinon si  VerifWin != 0  & toute les case = clické (decouverte)
+//        //return "Toute les case n'ont pas été decouverte "
+//    //sinon si case clicked = isbomb 
+//        //return "GAME OVER"
+//        //print "voulez vous recommencer :"
+//}
+
+
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
 // Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
