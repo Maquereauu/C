7c3ecbe29cadc0e3e02db4c60886462fe9dd36b0
unknown <gcharlesnicolai@gamingcampus.local>
2023-03-02 16:29:08 +0100

    test

--- C/C.c
+++ C/C.c
@@ -5,11 +5,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-//#include <SDL.h>
-//const int WIDTH = 800;
-//const int HEIGHT = 600;
-//const int POSITION_X = 100;
-//const int POSITION_Y = 100;
+#include <SDL.h>
+const int WIDTH = 800;
+const int HEIGHT = 600;
+const int POSITION_X = 100;
+const int POSITION_Y = 100;
 
 typedef struct Case {
 	int isBomb;
@@ -24,21 +24,180 @@ int getRandom();
 int verifWin();
 void revealNear();
 void Game();
+void CatchError();
 
 int main()
 {
-	int play = 1;
-	while (play) {
-		printf("%d", play);
-		Game(&play);
-	}
+	int choice;
+	puts("Terminal ou Graphique?");
+	if (scanf_s(" %d", &choice)) {
+		if (choice == 1) {
+			if (SDL_Init(SDL_INIT_EVERYTHING) < 0) { printf("%s\n", SDL_GetError()); exit(-1); }
+			SDL_Window* window;
+			window = SDL_CreateWindow("Hello SDL", POSITION_X, POSITION_Y, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
+			SDL_Rect carre_flag;
+			carre_flag.x = 10;
+			carre_flag.y = 10;
+			carre_flag.h = 5;
+			carre_flag.w = 5;
+			SDL_Rect rectangle_flag;
+			rectangle_flag.x = 10;
+			rectangle_flag.y = 10;
+			rectangle_flag.h = 10;
+			rectangle_flag.w = -2;
+			SDL_Rect back;
+			back.x = 200;
+			back.y = 50;
+			back.h = 500;
+			back.w = 400;
+			SDL_Rect rectangle_bomb_vertical;
+			rectangle_bomb_vertical.x = 22;
+			rectangle_bomb_vertical.y = 10;
+			rectangle_bomb_vertical.h = 10;
+			rectangle_bomb_vertical.w = 6;
 
+			SDL_Rect rectangle_bomb_horizontal;
+			rectangle_bomb_horizontal.x = 20;
+			rectangle_bomb_horizontal.y = 12;
+			rectangle_bomb_horizontal.h = 6;
+			rectangle_bomb_horizontal.w = 10;
+
+			SDL_Rect carre_bomb;
+			carre_bomb.x = 21;
+			carre_bomb.y = 11;
+			carre_bomb.h = 8;
+			carre_bomb.w = 8;
+			SDL_Rect meche_bomb;
+			meche_bomb.x = 24;
+			meche_bomb.y = 8;
+			meche_bomb.h = 2;
+			meche_bomb.w = 2;
+			SDL_Rect trait_bomb;
+			trait_bomb.x = 20;
+			trait_bomb.y = 14;
+			trait_bomb.h = 2;
+			trait_bomb.w = 10;
+
+			/*SDL_Rect chiffre_0;
+			SDL_Rect chiffre_1;
+			SDL_Rect chiffre_2;
+			SDL_Rect chiffre_3;
+			SDL_Rect chiffre_4;
+			SDL_Rect chiffre_5;
+			SDL_Rect chiffre_6;
+			SDL_Rect chiffre_7;
+			SDL_Rect chiffre_8;
+			SDL_Rect chiffre_9;*/
+
+
+
+
+
+
+
+
+
+
+
+			if (window == NULL) { printf("%s\n", SDL_GetError()); exit(-1); }
+			//dessiner un composant 
+			SDL_Event windowEvent;
+			SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+
+			while (1) {
+				//
+				if (SDL_PollEvent(&windowEvent)) {
+					if (windowEvent.type == SDL_QUIT) { break; }
+				}
+				//CHIFFRE
+				/*drawDigit(renderer, 0, 50, 50);
+				drawDigit(renderer, 1, 70, 50);
+				drawDigit(renderer, 2, 90, 50);
+				drawDigit(renderer, 3, 110, 50);
+				drawDigit(renderer, 4, 130, 50);
+				drawDigit(renderer, 5, 50, 70);
+				drawDigit(renderer, 6, 70, 70);
+				drawDigit(renderer, 7, 90, 70);
+				drawDigit(renderer, 8, 110, 70);
+				drawDigit(renderer, 9, 130, 70);*/
+				//CHIFFRE
+				//SDL_Renderer
+				SDL_SetRenderDrawColor(renderer, 169, 169, 169, 255);
+				SDL_RenderClear(renderer);
+
+				SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+				SDL_RenderFillRect(renderer, &back);
+
+
+				//BOMBE-----------------------
+				SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+				SDL_RenderFillRect(renderer, &rectangle_bomb_vertical);
+
+				SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+				SDL_RenderFillRect(renderer, &rectangle_bomb_horizontal);
+
+				SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+				SDL_RenderFillRect(renderer, &carre_bomb);
+
+				SDL_SetRenderDrawColor(renderer, 120, 120, 120, 255);
+				SDL_RenderFillRect(renderer, &trait_bomb);
+
+				SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+				SDL_RenderFillRect(renderer, &meche_bomb);
+				//BOMBE-----------------------
+
+
+				//FLAG------------------------
+				SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+				SDL_RenderFillRect(renderer, &rectangle_flag);
+
+				SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+				SDL_RenderFillRect(renderer, &carre_flag);
+				//FLAG----------------------
+				SDL_RenderPresent(renderer);
+
+			}
+			//SDL_Delay(5000);
+			SDL_DestroyRenderer(renderer);
+
+			SDL_DestroyWindow(window);
+			SDL_Quit();
+
+			return 0;
+		}
+		else {
+			int* play = 1;
+			while (play) {
+				Game(&play);
+			}
+		}
+	}
+else {
+	printf("merci de rentrer une valeur valide");
+	}
 }
 
 void Game(int* play) {
 	int difficulty;
+	int* InGame = 1;
 	puts("Veuillez choisir un mode de difficulté ");
-	scanf_s(" %d", &difficulty);
+	do {
+		if (scanf_s(" %d", &difficulty)) {
+			if (difficulty <= 0) {
+				printf("Merci de renseigner une valeur supérieure à 0\n");
+			}
+			else if(round(difficulty)==difficulty){
+				printf("mais , %d", &difficulty);
+				/*break;*/
+			}
+		}
+		else {
+			printf("Merci de renseigner un nombre entier \n");
+			while(getchar() != '\n');
+		}
+	} while (1);
+	printf("%d", difficulty);
 	srand(time(NULL));
 	int isError = 0;
 	Case** T = (Case**)malloc(sizeof(Case*) * (10 * difficulty));
@@ -61,6 +220,10 @@ void Game(int* play) {
 				free(T[i]);
 			}
 			free(T);
+			CatchError(play,&InGame);
+			if (InGame == 0) {
+				break;
+			}
 	
 		}
 		int flag = 0;
@@ -81,13 +244,20 @@ void Game(int* play) {
 		}
 		else {
 			puts("Quelle case voulez vous decouvrir ? :");
-			scanf_s("%d/%d", &playerchoicex, &playerchoicey);
-			if (T[playerchoicex][playerchoicey].isClicked != 1) {
-				T[playerchoicex][playerchoicey].isClicked = 1;
-				isError = 0;
-			}
-			else {
-				isError = 1;
+			while (1) {
+				if (scanf_s("%d/%d", &playerchoicex, &playerchoicey)) {
+					if (T[playerchoicex][playerchoicey].isClicked != 1) {
+						T[playerchoicex][playerchoicey].isClicked = 1;
+						isError = 0;
+					}
+					else {
+						isError = 1;
+					}
+				}
+				else {
+					printf("Merci de rentrer des coordonées valides(en nombre entier)");
+					printf("%d", &difficulty);
+				}
 			}
 		}
 	}
@@ -251,22 +421,26 @@ int verifWin(Case** T, int difficulty) {
 	return 0;
 }
 
-int CatchError(int* play) {
+void CatchError(int* play,int* InGame) {
 	int playAgain = 0;
 	puts("Voulez vous rejouer?");
-	if (scanf_s(" %d", &playAgain)) {
-		if (playAgain) {
-			return 1;
-		}
-		else {
-			*play = 0;
-			return 0;
+	while (1) {
+		if (scanf_s(" %d", &playAgain)) {
+			if (playAgain) {
+				*InGame = 0;
+				break;
+			}
+			else {
+				*InGame = 0;
+				*play = 0;
+				break;
 			}
 		}
 		else {
 			printf("Merci de rentrer un nombre entier");
 		}
 	}
+}
 
 void clrscr()
 {
